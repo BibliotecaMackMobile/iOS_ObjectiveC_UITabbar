32b86493c091cb1e48d96c86bd53793675f8a38e
Glauco Moraes <gmsm05@msn.com>
2014-10-02 19:34:25 -0300

    First Commit

---
+++ TabBar/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  TabBar
+//
+//  Created by Glauco Moraes on 02/10/14.
+//  Copyright (c) 2014 BEPiD. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
